b63a7a225f3ff7d143e843b51a7401e11349e7c0
DovydasG001 <dovydas.gricius@gmail.com>
2017-11-09 22:40:56 +0200

    Update Pixel.js

--- Model/Pixel.js
+++ Model/Pixel.js
@@ -1,25 +1,17 @@
 class Pixel {
   constructor() {
-    this.DEFAULT_WIDTH = 1;
-    this.DEFAULT_HEIGHT = 1;
-    this.DISABLED_COLOR = {r:127, g:127, b:127, a:255};
-    this.ENABLED_COLOR = {r:5, g:4, b:144, a:1};
+    this.DISABLED_COLOR = "#000000";
+    this.ENABLED_COLOR = "#000080";
     this.currentColor = this.ENABLED_COLOR;
   }
-
-  // Getters
-  getColor() {
-    return this.currentColor;
-  }
-
-  // Computed Getters
-  getColorString() {
-    return 'rgba('+ this.currentColor.r + ', ' + this.currentColor.g + ', ' +
-                 this.currentColor.b + ', ' + this.currentColor.a + ')';
-  }
-
-  // Setters
-  setColor(Color) {
-    this.currentColor = Color;
-  }
 }
+
+// Getters
+Pixel.prototype.getColor = function() {
+  return this.currentColor;
+};
+
+// Setters
+Pixel.prototype.setColor = function(Color) {
+  this.currentColor = Color;
+};
