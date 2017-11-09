76648f3a1f36a7a1ed87ceb48cd702f90cce6953
DovydasG001 <dovydas.gricius@gmail.com>
2017-11-09 22:39:51 +0200

    Update Drawing.js

--- Controller/Drawing.js
+++ Controller/Drawing.js
@@ -1,9 +1,91 @@
-class DrawingMethods {
-  constructor() {
+class ToolController {
+  constructor(view, layer, canvas) {
+    this.canvas = canvas;
+    this.layer = layer;
+    this.view = view;
+    this.currentTool = "pencil-tool";
+  }
+
+  setCurrentTool(toolName) {
+    this.currentTool = toolName;
+  }
+
+  // activates the current tool
+  CurrentTool(eventType, mousePosition) {
+    // Currently it only activates the PencilTool
+    switch (this.currentTool) {
+      case "pencil-tool":
+        ToolController.PencilTool(eventType, mousePosition, this);
+        break;
+      case "bucket-tool":
+        ToolController.BucketTool(eventType, mousePosition, this);
+        break;
+      default:
+        console.log("No such tool!");
+    }
+    //ToolController.PencilTool(eventType, mousePosition, this);
+  }
+
+  static BucketTool(eventType, mousePosition, object) {
+    if (eventType == 'leftbuttondown') {
+      ToolController.paintWholeLayer(
+        "#" + object.view.brushColor,
+        object.layer
+      );
+      object.view.refresh(
+        View.findBounds(
+          {x: 0, y: 0},
+          {x: object.layer.width, y: object.layer.height},
+          object.layer.width,
+          object.layer.height
+        ),
+        object.view.pixelSize
+      );
+    }
+  }
+
+  static PencilTool(eventType, mousePosition, object) {
+    // Checks if the event was a single click or a click and move
+    if (eventType == 'leftbuttondown') {
+      // Calls appropriate drawing method
+      ToolController.drawPoint(
+        "#" + object.view.brushColor,
+        object.layer,
+        mousePosition.x,
+        mousePosition.y
+      );
+      // Refreshes the View to show the change
+      object.view.refresh(
+        View.findBounds(
+          mousePosition,
+          mousePosition,
+          object.layer.width,
+          object.layer.height
+        ),
+        object.view.pixelSize
+      );
+    } else if (eventType == 'mousemove') {
+      ToolController.drawLine(
+        "#" + object.view.brushColor,
+        object.layer,
+        mousePosition.xLast,
+        mousePosition.yLast,
+        mousePosition.xNew,
+        mousePosition.yNew
+      );
+      object.view.refresh(
+        View.findBounds(
+          {x: mousePosition.xLast, y: mousePosition.yLast},
+          {x: mousePosition.xNew, y: mousePosition.yNew},
+          object.layer.width,
+          object.layer.height
+        ),
+        object.view.pixelSize
+      );
+    }
   }
 
   static drawPoint(color, layer, x, y) {
-    // console.log(layer);
     layer.getPixelAt(x, y).setColor(color);
   }
 
@@ -15,8 +97,7 @@ class DrawingMethods {
    var err = dx-dy;
 
    while(true){
-     DrawingMethods.drawPoint(color, layer, x0, y0);  // Do what you need to for this
-
+     ToolController.drawPoint(color, layer, x0, y0);
      if ((x0==x1) && (y0==y1)) break;
      var e2 = 2*err;
      if (e2 >-dy){ err -= dy; x0  += sx; }
@@ -26,6 +107,7 @@ class DrawingMethods {
 
   //Changes the color of all of the layer
   static paintWholeLayer(color, layer) {
+    console.log("I run!");
     for (var row = 0; row < layer.getHeight(); row++) {
       for (var column = 0; column < layer.getWidth(); column++) {
         layer.getPixelAt(column, row).setColor(color);
@@ -33,8 +115,4 @@ class DrawingMethods {
     }
   }
 
-  static colorStringToObj(colorString) {
-    var color = colorString.split(", ");
-    return {r:color[0], g:color[1], b:color[2], a:color[3],};
-  }
 }
